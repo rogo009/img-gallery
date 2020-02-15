819e2e341931c1e91cb682e055d0093bf7e352dd
robin <rogo009git@gmail.com>
2020-02-15 15:56:29 -0800

    add animation

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const current = document.querySelector("#current");
 const imgs = document.querySelectorAll(".imgs img");
-const opacity = 0.4;
+const opacity = 0.6;
+
+// set first img opacity
+imgs[0].style.opacity = opacity;
 
 // loop through each img and add event listener onto each img
 
@@ -15,6 +18,12 @@ imgs.forEach(img => img.addEventListener("click", imgClick));
 function imgClick(e) {
   // reset opacity
   imgs.forEach(img => (img.style.opacity = 1));
+
+  // add fade in class for animation
+  current.classList.add("fade-in");
+
+  // remove fade in class after .5s
+  setTimeout(() => current.classList.remove("fade-in"), 500);
 
   // change current img to src of clicked img
   current.src = e.target.src;
